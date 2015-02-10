78fe354d44bac22ebdfc9c78c03f1325435a03e3
ssut <ssut@ssut.me>
2015-02-10 22:35:56 +0900

    Improve log message to better describe

--- pushbank/mail.py
+++ pushbank/mail.py
@@ -11,7 +11,6 @@ from email.header import Header
 session = None
 connect_params = {}
 
-
 def connect(server='smtp.gmail.com', port=587, user='', passwd='', tls=False):
     params = locals()
     global session, connect_params
@@ -23,17 +22,18 @@ def connect(server='smtp.gmail.com', port=587, user='', passwd='', tls=False):
         result = session.login(user, passwd)
         # check smtp login status (235, 270 = accepted)
         if result[0] != 235 and result[0] != 270 and 'Accept' not in result[1]:
-            raise Exception('SMTP verification failed')
+            raise Exception(
+                'The connection to the SMTP server was unsuccessful.')
         # check smtp connection status
         status = session.noop()[0]
         if status != 250:
-            raise Exception('SMTP connection closed')
+            raise Exception('The connection to the SMTP server was lost.')
     except:
-        logging.error('SMTP login failed')
+        logging.error('Unable to logon to the SMTP server.')
         sys.exit(1)
     else:
         connect_params = params
-        logging.info('SMTP login success')
+        logging.info('Successfully connected to the SMTP server.')
         return True
 
 
@@ -55,6 +55,7 @@ def send(target, title, content, adapter_name):
     corpo['To'] = target
     corpo['Subject'] = Header(title, 'utf-8')
     session.sendmail(target, [target], corpo.as_string())
-    logging.info('Sent an email by %s', adapter_name)
+    logging.info('The mail has been sent successfully originated from "%s".',
+        adapter_name)
     # delay
-    sleep(1)
+    eventlet.sleep(0.5)
